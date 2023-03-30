d2cf97c8c7e0f9efbf3680555316222799c6fc03
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-03-29 21:06:44 -0300

    denuevo

--- js/main.js
+++ js/main.js
@@ -263,7 +263,7 @@ function cargarProductos(productosElegidos) {
 		const div = document.createElement("div");
 		div.classList.add("producto");
 		div.innerHTML = `
-            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <img class="producto-imagen" src=".${producto.imagen}" alt="${producto.titulo}">
             <div class="detalles">
                 <h3 class="producto-titulo">${producto.titulo}</h3>
                 <p class="producto-precio">$${producto.precio}</p>
